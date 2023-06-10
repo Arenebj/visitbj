dae6be1729d4c181f1c076658e96f0d6efc54d91
alakekarimou <alakekarimou@gmail.com>
2023-06-10 17:13:53 +0100

    modify appbar of home page

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -1,1 +1,8 @@
 export 'splashScreen/splahScreen.dart';
+export 'package:visitbj/ressources/theme/app_theme.dart';
+export 'package:visitbj/ui/common/home/section/section_event.dart';
+export 'package:visitbj/ui/common/home/section/section_party.dart';
+export 'package:visitbj/ui/common/home/section/section_sejour.dart';
+export 'package:visitbj/ui/common/home/section/section_welcome.dart';
+export 'package:visitbj/ui/pages/homeScreen/home_appbar.dart';
+export 'package:visitbj/ui/pages/homeScreen/home_body.dart';
